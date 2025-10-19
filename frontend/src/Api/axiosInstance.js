d9e91a5ff91a35c66a7d0237be54d7fcf45ce7bb
rinshafebin <rinshafebinkk12@gmail.com>
2025-10-19 19:48:06 +0530

    context created for Auth

--- frontend/src/Api/axiosInstance.js
+++ frontend/src/Api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL; 
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
---
+++ frontend/src/Context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const login = (userData, token) => {
+    setToken(token);
+    setUser(userData);
+    localStorage.setItem("token", token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
